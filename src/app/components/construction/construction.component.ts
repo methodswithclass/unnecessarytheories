5f64034b1ca232df494778bc9b8c343ef2c57669
Christopher Polito <chris@polito.io>
2017-04-14 19:58:42 -0400

    update

--- src/app/components/construction/construction.component.ts
+++ src/app/components/construction/construction.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { ViewEncapsulation, Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-construction',
-  templateUrl: './construction.component.html',
-  styleUrls: ['./construction.component.css']
+	selector: 'app-construction',
+	templateUrl: './construction.component.html',
+	styleUrls: [
+		'./construction.component.css',
+		'../../assets/css/museo/museo300.css',
+		'./../../assets/css/styles.css'
+	]
+	// styleUrls:["http://code.methodswithclass.com/api/classes.css"],
+	// encapsulation: ViewEncapsulation.None,
 })
 export class ConstructionComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
