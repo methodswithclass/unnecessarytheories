46c5526d855e1951360380d91203474118c0ec2f
Christopher Polito <chris@polito.io>
2017-06-07 22:40:12 -0400

    update

--- server.js
+++ server.js
@@ -6,21 +6,22 @@ const app = express();
 // // a protocol other than HTTPS,
 // // redirect that request to the
 // // same url but with HTTPS
-// const forceSSL = function() {
-//   return function (req, res, next) {
-//     if (req.headers['x-forwarded-proto'] !== 'https') {
-//       return res.redirect(
-//        ['https://', req.get('Host'), req.url].join('')
-//       );
-//     }
-//     next();
-//   }
-// }
+const forceSSL = function() {
+  return function (req, res, next) {
+  	console.log("force https");
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
 // // Instruct the app
 // // to use the forceSSL
 // // middleware
-// app.use(forceSSL());
-
+if (process.env.NODE_ENV == "production") app.use(forceSSL());
+else {console.log("environment development")}
 
 app.use(express.static(__dirname + "/dist"));
 
