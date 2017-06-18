37c18a8c9f4fde815fc6cb7165eb394f5a021a19
Christopher Polito <chris@polito.io>
2017-06-17 23:04:20 -0400

    update

--- server/bot.js
+++ server/bot.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 const variables = require("./variables");
 
-var debugCrawler = false;
+var debugCrawler = true;
 
 var meta = JSON.parse(fs.readFileSync('data.json', 'utf8'));
 
