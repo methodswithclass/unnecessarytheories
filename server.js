0004ef0fab5a1bb6692c5a3cc4f702fe1a99cf21
Christopher Polito <chris@polito.io>
2017-06-18 13:02:58 -0400

    update

--- server.js
+++ server.js
@@ -21,13 +21,13 @@ app.set('views', path.join(__dirname, '/server'));
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-app.use("/assets/css", express.static(path.join(__dirname, "dist/assets/css")));
-app.use("/assets/css/museo", express.static(path.join(__dirname, "dist/assets/css/museo")));
-app.use("/assets/js", express.static(path.join(__dirname, "dist/assets/js")));
-app.use("/assets/img", express.static(path.join(__dirname, "dist/assets/img")));
-// app.use("/dir", express.directory(__dirname + "/"));
-app.use('/', serveIndex('/', {'icons': true}))
-app.use("/blog/*", express.static(path.join(__dirname, "dist")));
+app.use("/assets/css", express.static(path.join("app/dist/assets/css")));
+app.use("/assets/css/museo", express.static(path.join("app/dist/assets/css/museo")));
+app.use("/assets/js", express.static(path.join("app/dist/assets/js")));
+app.use("/assets/img", express.static(path.join("app/dist/assets/img")));
+// app.use("/", express.static(path.join(__dirname + "/")));
+app.use('/', serveIndex(__dirname + '/', {'icons': true}))
+app.use("/blog/*", express.static(path.join(__dirname, "app/dist")));
 
 
 var listener = app.listen(process.env.PORT || 8080, function () {
