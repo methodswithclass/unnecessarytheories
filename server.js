6011164ce6bee1730f1aca6b23e920240c180fcb
Christopher Polito <chris@polito.io>
2017-06-17 23:11:33 -0400

    update

--- server.js
+++ server.js
@@ -19,12 +19,12 @@ app.use(middleware.refresh());
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-app.use("/dist/assets/css", express.static(path.join(__dirname, "dist/assets/css")));
-app.use("/dist/assets/css/museo", express.static(path.join(__dirname, "dist/assets/css/museo")));
-app.use("/dist/assets/js", express.static(path.join(__dirname, "dist/assets/js")));
-app.use("/dist/assets/img", express.static(path.join(__dirname, "dist/assets/img")));
 app.use("/", express.static(path.join(__dirname, "dist")));
 app.use("/blog/*", express.static(path.join(__dirname, "dist")));
+// app.use("/dist/assets/css", express.static(path.join(__dirname, "dist/assets/css")));
+// app.use("/dist/assets/css/museo", express.static(path.join(__dirname, "dist/assets/css/museo")));
+// app.use("/dist/assets/js", express.static(path.join(__dirname, "dist/assets/js")));
+// app.use("/dist/assets/img", express.static(path.join(__dirname, "dist/assets/img")));
 
 
 var listener = app.listen(process.env.PORT || 8080, function () {
