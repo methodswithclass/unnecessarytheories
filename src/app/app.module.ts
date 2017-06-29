48114e669cecb0030c7c76568f30e130798bea48
Christopher Polito <chris@polito.io>
2017-06-29 08:50:26 -0400

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Router, Routes, NavigationStart, RouterOutletMap } from "@angular/router";
+import { RouterModule, Router, Routes, NavigationStart } from "@angular/router";
 // import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
