2b8e2f8b4c0efa0fa038b8f31a3a75d023cdd32b
Christopher Polito <chris@polito.io>
2017-06-29 12:17:48 -0400

    update merge

--- server.js
+++ server.js
@@ -1,53 +1,39 @@
 const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+
 const app = express();
 
-
-var refreshPages = [
-"construction"
-]
+const bot = require("./server/bot");
+const middleware = require("./server/middleware");
 
 
-// // If an incoming request uses
-// // a protocol other than HTTPS,
-// // redirect that request to the
-// // same url but with HTTPS
-const forceSSL = function() {
-  return function (req, res, next) {
-  	console.log("force https");
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-                          ['https://', req.get('Host'), req.url].join('')
-                          );
-    }
-    next();
-  }
-}
+
+app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, '/server'));
 
 
-var refresh = function () {
-
-	return function (req, res, next) {
-
-		console.log(req.url);
-
-		var urlArray = req.url.split("/");
-
-		for (var i in refreshPages) {
-			if (urlArray[1] == refreshPages[i]) {
-				return res.redirect(['http://', req.get('Host')].join(''));
-			}
-		}
-
-		next();
-
-	}
-}
-
-app.use(refresh());
-// if (process.env.NODE_ENV == "production") app.use(forceSSL());
-// else {console.log("environment development")}
-
-app.use(express.static(__dirname + "/dist"));
+app.use(bot.middleware);
+app.use(middleware.refresh());
+// if  (process.env.NODE_ENV == "production") app.use(middleware.forceSSL());
+// else {console.log("environment development");}
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(express.static(path.join(__dirname, "dist/assets/css")));
+app.use(express.static(path.join(__dirname, "dist/assets/css/museo")));
+app.use(express.static(path.join(__dirname, "dist/assets/js")));
+app.use(express.static(path.join(__dirname, "dist/assets/img")));
+app.use("/", express.static(path.join(__dirname, "dist")));
+app.use("/blog/*", express.static(path.join(__dirname, "dist")));
 
 
-app.listen(process.env.PORT || 8080);
+var listener = app.listen(process.env.PORT || 8080, function () {
+
+	// console.log("dirname", __dirname);
+
+	console.log("listening on port", listener.address().port);
+});
+
+
+
---
+++ server/variables.js
@@ -0,0 +1,10 @@
+module.exports = {
+	url:{
+		dev:"localhost:8080",
+		prod:"http://www.methodswc.com"
+	},
+	FBappID:{
+		prod:'696572137111194',
+		dev:'725062234262184'
+	}
+}
