347862df42e2ac7ae29a9417de8d1502b4d6e3cf
Christopher Polito <chris@polito.io>
2017-07-08 11:56:46 -0400

    update

--- server/middleware.js
+++ server/middleware.js
@@ -42,9 +42,9 @@ var accessControl = function () {
 		// Website you wish to allow to connect
     	// res.setHeader('Access-Control-Allow-Origin', ['http://unecessarytheories-dev.herokuapp.com', 'http://unecessarytheories.herokuapp.com', 'http://localhost:8080']);
 
-    	res.setHeader("Access-Control-Allow-Origin", "*");
+    	return res.setHeader("Access-Control-Allow-Origin", "*");
 
-    	next();
+    	// next();
 	}
 }
 
